7161b8e74ec2b8293845ddde6a99ac97f387411c
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-11-25 18:56:21 +0300

    Remove by pod statement

--- internal/queries.go
+++ internal/queries.go
@@ -1,6 +1,6 @@
 package internal
 
-const queryCPU = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=\"%s\"}) by (pod)"
+const queryCPU = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=\"%s\"})"
 
 //get running pods group by namespace
 const queryPodsByNamespace = "count(kube_pod_info) by (namespace)"
@@ -9,7 +9,7 @@ const queryPodsByNamespace = "count(kube_pod_info) by (namespace)"
 const queryAllPods = "count(kube_pod_info)"
 
 //Workload Memory Utilization
-const queryMemory = "sum(container_memory_working_set_bytes{namespace=\"%s\"}) by (pod)"
+const queryMemory = "sum(container_memory_working_set_bytes{namespace=\"%s\"})"
 
 var queryDict = map[string]string{
 	"cpu":             queryCPU,
