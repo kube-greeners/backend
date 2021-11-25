89bccb6347566d994e8281983d6afb9ca56d32f0
albandelishi <alban.delishi97@gmail.com>
2021-11-25 19:49:30 +0100

    changes on var names and at server go

--- internal/queries.go
+++ internal/queries.go
@@ -1,19 +1,19 @@
 package internal
 
-const queryCPU = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=\"%s\"})"
+const cpu_usage = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=\"%s\"}) by (pod)"
 
-//get running pods group by namespace
-const queryPodsByNamespace = "count(kube_pod_info) by (namespace)"
+//get the number of running pods group by namespace
+const active_pods_by_namespace = "count(kube_pod_info) by (namespace)"
 
 //get all runnnig pods
-const queryAllPods = "count(kube_pod_info)"
+const all_active_pods = "count(kube_pod_info)"
 
-//Workload Memory Utilization
-const queryMemory = "sum(container_memory_working_set_bytes{namespace=\"%s\"})"
+//Workload Memory working
+const memory_usage = "sum(container_memory_working_set_bytes{namespace=\"%s\"}) by (pod)"
 
 var queryDict = map[string]string{
-	"cpu":             queryCPU,
-	"pods":            queryAllPods,
-	"podsByNamespace": queryPodsByNamespace,
-	"memory":          queryMemory,
+	"cpu_usage":                cpu_usage,
+	"all_active_pods":          all_active_pods,
+	"active_pods_by_namespace": active_pods_by_namespace,
+	"memory_usage":             memory_usage,
 }
