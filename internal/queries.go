3d782b0255fb1eefbb782742c1aa219662a6e2fd
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-11-28 21:12:36 +0100

    feat: more explanatory co2 query fix: remove unneccesary query fix: experiment with real memory queries

--- internal/queries.go
+++ internal/queries.go
@@ -1,55 +1,55 @@
 package internal
 
-const cpu_usage = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=\"%s\"}) by (pod)"
+import (
+	"fmt"
+)
 
-//get the number of running pods group by namespace
-const active_pods_by_namespace = "count(kube_pod_info) by (namespace)"
-
-//get all runnnig pods
+//get all running pods
 const all_active_pods = "count(kube_pod_info)"
 
+// Ozan: sum((container_memory_usage_bytes{namespace!=""} - container_memory_working_set_bytes{namespace!=""}) / 1024 / 1024) by (namespace)
+// If you look, usage_bytes are bigger than working_set_bytes
+
 //Workload Memory working
-const memory_allocation = "sum(container_memory_working_set_bytes{namespace=\"%s\"}) by (pod)"
+const memory_allocation = "sum(container_memory_usage_bytes{namespace!=\"\"})"
 
 //Memory usage
-const memory_usage = "container_memory_working_set_bytes{pod_name=~\"compute-.*\", image!=\"\", container_name!=\"POD\"}"
+const memory_usage = "sum(container_memory_working_set_bytes{namespace!=\"\"})"
 
 //CPU allocation
-const cpu_allocation = "avg(kube_pod_container_resource_limits_cpu_cores{pod=~\"compute-.*\"})"
+const cpu_allocation = "avg(kube_pod_container_resource_limits_cpu_cores{})"
 
-/******* CO2 EMISSION FORMULA block ******/
-
-const cloud_provider_usage = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate)"
+const cpu_usage = "sum(rate(container_cpu_usage_seconds_total[6h]))"
 
 //calculate cloud energy conversion factors [kWh]
+// https://github.com/cloud-carbon-footprint/cloud-carbon-footprint/blob/trunk/packages/gcp/src/domain/GcpFootprintEstimationConstants.ts
 const min_watts_coeficient = 0.71
 const max_watts_coeficient = 4.26
 const avg_cpu_utilization = 0.5 //50%
 
 const avg_watts = min_watts_coeficient + avg_cpu_utilization*(max_watts_coeficient-min_watts_coeficient)
 
-//cpu utilization per hour
-const cpu_utilization_per_hour = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate) / 3600"
-const compute_watts_hours = avg_watts * avg_cpu_utilization
+// Utilization of CPU time for the last week summed up
+const cpu_utilization_per_hour = "sum(avg_over_time(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate[7d])) / 3600"
 
-//power usage effectiveness coeficient
-const pue_coeficient = 1.1
+const pue_coeficient = 1.1 // GKE
 
-//grid emission factors coeficient for europe-west1 zone
-const emission_factors_coeficient = 0.000196
+const emission_factors_coeficient = 0.000196 * 1000 // europe-west1
+// Multiply by 1000 because, the factor is in metric tons
 
 //co2 formula but it has problem with string and float
 //const co2_emission = cloud_provider_usage * compute_watts_hours * pue_coeficient * emission_factors_coeficient
 
 //co2 formula
-const co2_emission = "(sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate)) * (0.71 + 0.5 * (4.26-0.71)) * (sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate) / 3600) * 1.1 * 0.000196"
+var co2_emission = fmt.Sprintf("(%s) * (%f * (%s)) * %f * %f", cpu_usage, avg_watts, cpu_utilization_per_hour, pue_coeficient, emission_factors_coeficient)
+
+// const co2_emission = "(sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate)) * (0.71 + 0.5 * (4.26-0.71)) * (sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate) / 3600) * 1.1 * 0.000196"
 
 var queryDict = map[string]string{
-	"cpu_usage":                cpu_usage,
-	"all_active_pods":          all_active_pods,
-	"active_pods_by_namespace": active_pods_by_namespace,
-	"memory_allocation":        memory_allocation,
-	"memory_usage":             memory_usage,
-	"cpu_allocation":           cpu_allocation,
-	"co2_emission":             co2_emission,
+	"cpu_usage":         cpu_usage,
+	"all_active_pods":   all_active_pods,
+	"memory_allocation": memory_allocation,
+	"memory_usage":      memory_usage,
+	"cpu_allocation":    cpu_allocation,
+	"co2_emission":      co2_emission,
 }
