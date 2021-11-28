5f2f9bd2d4fdb205f6ae9bc379db4a2681ebd7b8
Alban Delishi <alban.delishi97@gmail.com>
2021-11-28 15:59:53 +0100

    co2 emission formula

--- internal/queries.go
+++ internal/queries.go
@@ -17,6 +17,33 @@ const memory_usage = "container_memory_working_set_bytes{pod_name=~\"compute-.*\
 //CPU allocation
 const cpu_allocation = "avg(kube_pod_container_resource_limits_cpu_cores{pod=~\"compute-.*\"})"
 
+/******* CO2 EMISSION FORMULA block ******/
+
+const cloud_provider_usage = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate)"
+
+//calculate cloud energy conversion factors [kWh]
+const min_watts_coeficient = 0.71
+const max_watts_coeficient = 4.26
+const avg_cpu_utilization = 0.5 //50%
+
+const avg_watts = min_watts_coeficient + avg_cpu_utilization*(max_watts_coeficient-min_watts_coeficient)
+
+//cpu utilization per hour
+const cpu_utilization_per_hour = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate) / 3600"
+const compute_watts_hours = avg_watts * avg_cpu_utilization
+
+//power usage effectiveness coeficient
+const pue_coeficient = 1.1
+
+//grid emission factors coeficient for europe-west1 zone
+const emission_factors_coeficient = 0.000196
+
+//co2 formula but it has problem with string and float
+//const co2_emission = cloud_provider_usage * compute_watts_hours * pue_coeficient * emission_factors_coeficient
+
+//co2 formula
+const co2_emission = "(sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate)) * (0.71 + 0.5 * (4.26-0.71)) * (sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate) / 3600) * 1.1 * 0.000196"
+
 var queryDict = map[string]string{
 	"cpu_usage":                cpu_usage,
 	"all_active_pods":          all_active_pods,
@@ -24,4 +51,5 @@ var queryDict = map[string]string{
 	"memory_allocation":        memory_allocation,
 	"memory_usage":             memory_usage,
 	"cpu_allocation":           cpu_allocation,
+	"co2_emission":             co2_emission,
 }
