03ac0569026d414601a2e6e6a6bb4a552f9cddea
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-11-28 22:20:00 +0100

    feat: add memory to CO2 emissions fix: return Gb of memory from the endpoint fix: error at decreasing during bad hours

--- internal/queries.go
+++ internal/queries.go
@@ -11,10 +11,10 @@ const all_active_pods = "count(kube_pod_info)"
 // If you look, usage_bytes are bigger than working_set_bytes
 
 //Workload Memory working
-const memory_allocation = "sum(container_memory_usage_bytes{namespace!=\"\"})"
+const memory_allocation = "sum(container_memory_usage_bytes{namespace!=\"\"}) / 1024 / 1024 / 1024" // 1GB = 1024 * 1024 * 1024 Bytes
 
 //Memory usage
-const memory_usage = "sum(container_memory_working_set_bytes{namespace!=\"\"})"
+const memory_usage = "sum(container_memory_working_set_bytes{namespace!=\"\"}) / 1024 / 1024 / 1024"
 
 //CPU allocation
 const cpu_allocation = "avg(kube_pod_container_resource_limits_cpu_cores{})"
@@ -34,31 +34,38 @@ const cpu_utilization_per_hour = "sum(avg_over_time(node_namespace_pod_container
 
 const pue_coeficient = 1.1 // GKE
 
-const emission_factors_coeficient = 0.000196 * 1000 // europe-west1
-// Multiply by 1000 because, the factor is in metric tons
+const emission_factors_coeficient = 0.000196 * 1000 * 1000 // europe-west1
+// Multiply by 1000 * 1000 because the factor is in metric tons and we want grams
 
 //co2 formula but it has problem with string and float
 //const co2_emission = cloud_provider_usage * compute_watts_hours * pue_coeficient * emission_factors_coeficient
 
 //co2 formula
-var co2_emission = fmt.Sprintf("(%s) * (%f * (%s)) * %f * %f", cpu_usage, avg_watts, cpu_utilization_per_hour, pue_coeficient, emission_factors_coeficient)
+var cpu_co2_emission = fmt.Sprintf("(%s) * (%f * (%s)) * %f * %f", cpu_usage, avg_watts, cpu_utilization_per_hour, pue_coeficient, emission_factors_coeficient)
+
+const memory_coefficient = 0.000392 // GKE
+var memory_co2_emission = fmt.Sprintf("(%s) * %f", memory_usage, memory_coefficient)
+
+var co2_emission = fmt.Sprintf("(%s + %s) * %f * %f", cpu_co2_emission, memory_co2_emission, pue_coeficient, emission_factors_coeficient)
 
 // const co2_emission = "(sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate)) * (0.71 + 0.5 * (4.26-0.71)) * (sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate) / 3600) * 1.1 * 0.000196"
 
 func decreaseAtCertainHour(startHour int, endHour int, query string, scaleCoef float32) string {
 	newQuery := fmt.Sprintf(
-		"(%s and (absent(hour() < %d) or absent(hour() > %d))) or (%s * %f and (absent(hour() > %d) or absent(hour() < %d)))",
-		query, startHour, endHour, query, scaleCoef, startHour, endHour)
+		"(%s * %f and  (absent(hour() < %d) or absent(hour() > %d))) or (%s and (absent(hour() > %d) or absent(hour() < %d)))",
+		query, scaleCoef, startHour, endHour, query, startHour, endHour)
 	return newQuery
 }
 
+var co2_emission_with_kube_green = decreaseAtCertainHour(17, 6, co2_emission, 0.7)
+var saved_co2_emissions = fmt.Sprintf("(%s) - (%s)", co2_emission, co2_emission_with_kube_green)
 var queryDict = map[string]string{
-	"cpu_usage":         cpu_usage,
-	"all_active_pods":   all_active_pods,
-	"memory_allocation": memory_allocation,
-	"memory_usage":      memory_usage,
-	"cpu_allocation":    cpu_allocation,
-	"co2_emission":      co2_emission,
-	"saved_cpu_at_night": decreaseAtCertainHour(17, 6, queryCPU, 0.7),
-	"saved_memory_at_night": decreaseAtCertainHour(17, 6, queryMemory, 0.8),
+	"cpu_usage":                    cpu_usage,
+	"all_active_pods":              all_active_pods,
+	"memory_allocation":            memory_allocation,
+	"memory_usage":                 memory_usage,
+	"cpu_allocation":               cpu_allocation,
+	"co2_emission":                 co2_emission,
+	"co2_emission_with_kube_green": co2_emission_with_kube_green,
+	"saved_co2_emission":           saved_co2_emissions,
 }
