fc403de5df14a2368e367199c9b01dc70d265abd
Zoé <zoepesleuxzp@gmail.com>
2021-12-01 17:13:42 +0100

    first draft of prometheus_test

---
+++ internal/prometheus_test.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"testing"
+)
+
+func TestPrometheusClient(t *testing.T) {
+
+}
+
+func TestRawQuery(t *testing.T) {
+
+}
+
+func TestParseInterval(t *testing.T) {
+
+}
+
+func TestExecuteQuery(t *testing.T) {
+
+}
