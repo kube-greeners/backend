0feed7dae43c7de93539fae0f9279b307b9f852e
Alban Delishi <alban.delishi97@gmail.com>
2021-12-01 23:55:30 +0100

    server.go test file

---
+++ internal/server_test.go
@@ -0,0 +1,17 @@
+package internal
+
+import "testing"
+
+//tutorial
+//https://medium.com/rungo/unit-testing-made-easy-in-go-25077669318
+//https://www.youtube.com/watch?v=sOeUf1YICSA&t=49s
+//https://betterprogramming.pub/easy-guide-to-unit-testing-in-golang-4fc1e9d96679
+
+//test template
+func TestParameter(t *testing.T) {
+test := []struct{
+	namespace string
+	timeInterval string
+	step string
+}
+}
