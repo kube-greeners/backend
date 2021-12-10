badedc8c1ae9cce80a424836e238e0790ce2e7fc
Alban Delishi <alban.delishi97@gmail.com>
2021-12-10 19:13:48 +0100

    added the namespace parameter

--- internal/queries.go
+++ internal/queries.go
@@ -5,21 +5,18 @@ import (
 )
 
 //get all running pods
-const all_active_pods = "count(kube_pod_info)"
-
-// Ozan: sum((container_memory_usage_bytes{namespace!=""} - container_memory_working_set_bytes{namespace!=""}) / 1024 / 1024) by (namespace)
-// If you look, usage_bytes are bigger than working_set_bytes
+const all_active_pods = "count(kube_pod_info{namespace=~\"%s\"})"
 
 //Workload Memory working
 const memory_allocation = "sum(container_memory_usage_bytes{namespace=~\"%s\"}) / 1024 / 1024 / 1024" // 1GB = 1024 * 1024 * 1024 Bytes
 
 //Memory usage
-const memory_usage = "sum(container_memory_working_set_bytes{namespace!=\"\"}) / 1024 / 1024 / 1024"
+const memory_usage = "sum(container_memory_working_set_bytes{namespace=~\"%s\"}) / 1024 / 1024 / 1024"
 
 //CPU allocation
-const cpu_allocation = "sum(namespace_cpu:kube_pod_container_resource_requests:sum)"
+const cpu_allocation = "sum(namespace_cpu:kube_pod_container_resource_requests:sum{namespace=~\"%s\"})"
 
-const cpu_usage = "sum(rate(container_cpu_usage_seconds_total[6h]))"
+const cpu_usage = "sum(rate(container_cpu_usage_seconds_total[6h]{namespace=~\"%s\"}))"
 
 //calculate cloud energy conversion factors [kWh]
 // https://github.com/cloud-carbon-footprint/cloud-carbon-footprint/blob/trunk/packages/gcp/src/domain/GcpFootprintEstimationConstants.ts
@@ -30,7 +27,7 @@ const avg_cpu_utilization = 0.5 //50%
 const avg_watts = min_watts_coeficient + avg_cpu_utilization*(max_watts_coeficient-min_watts_coeficient)
 
 // Utilization of CPU time for the last week summed up
-const cpu_utilization_per_hour = "sum(avg_over_time(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate[7d])) / 3600"
+const cpu_utilization_per_hour = "sum(avg_over_time(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=~\"%s\"}[7d])) / 3600"
 
 const pue_coeficient = 1.1 // GKE
 
@@ -59,7 +56,7 @@ func sum_over_time_and_step(query string, time string, step string) string {
 	return fmt.Sprintf("sum_over_time((%s)[%s:%s])", query, time, step)
 }
 
-const namespace_names = "kube_namespace_labels"
+const namespace_names = "sum(kube_namespace_labels) by namespace"
 
 var queryDict = map[string]string{
 	"cpu_usage":                    cpu_usage,
