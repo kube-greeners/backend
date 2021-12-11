96980c4bcbaf310c13e21db9477086c2a7f50b73
Alban Delishi <alban.delishi97@gmail.com>
2021-12-11 21:07:50 +0100

    restore the replace query -_-

--- internal/prometheus.go
+++ internal/prometheus.go
@@ -3,9 +3,9 @@ package internal
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"os"
 	"strconv"
+	s "strings"
 	"time"
 
 	"github.com/prometheus/client_golang/api"
@@ -63,10 +63,10 @@ func (client prometheus) rawQuery(query string, start time.Time, end time.Time, 
 }
 
 func (client prometheus) executeQuery(query string, parameters queryParameters) (string, error) {
-	// for s.Contains(query, "\"%s\"") {
-	// 	query = s.Replace(query, "%s", parameters.namespace, 1)
-	// }
-	fmt.Printf("the parameter %s", parameters.start)
+	for s.Contains(query, "\"%s\"") {
+		query = s.Replace(query, "%s", parameters.namespace, 1)
+	}
+
 	intStart, err := strconv.ParseInt(parameters.start, 0, 0)
 	timestampStart := time.Unix(intStart/1000, 0)
 	if err != nil {
