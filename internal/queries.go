0721dcce484f52991e69f7464f21eb2c84c78fca
Zoé <zoepesleuxzp@gmail.com>
2022-01-02 14:51:17 +0100

    added co2 calculation when KG is not running

--- internal/queries.go
+++ internal/queries.go
@@ -18,6 +18,9 @@ const cpu_allocation = "sum(namespace_cpu:kube_pod_container_resource_requests:s
 
 const cpu_usage = "sum(rate(container_cpu_usage_seconds_total{namespace=~\"%s\"}[6h]))"
 
+// return 1 when kube-green is not running, empty otherwise
+const kg_not_running = "absent(max(kube_green_replicas_sleeping)>0)"
+
 //calculate cloud energy conversion factors [kWh]
 // https://github.com/cloud-carbon-footprint/cloud-carbon-footprint/blob/trunk/packages/gcp/src/domain/GcpFootprintEstimationConstants.ts
 const min_watts_coeficient = 0.71
@@ -34,23 +37,38 @@ const pue_coeficient = 1.1 // GKE
 const emission_factors_coeficient = 0.000196 * 1000 * 1000 // europe-west1
 // Multiply by 1000 * 1000 because the factor is in metric tons and we want grams
 
+const memory_coefficient = 0.000392 // GKE
+
 //co2 formula
 var cpu_co2_emission = fmt.Sprintf("(%s) * (%f * (%s)) * %f * %f", cpu_usage, avg_watts, cpu_utilization_per_hour, pue_coeficient, emission_factors_coeficient)
 
-const memory_coefficient = 0.000392 // GKE
 var memory_co2_emission = fmt.Sprintf("(%s) * %f", memory_usage, memory_coefficient)
 
 var co2_emission = fmt.Sprintf("(%s + %s) * %f * %f", cpu_co2_emission, memory_co2_emission, pue_coeficient, emission_factors_coeficient)
 
+///////////////// Co2 calculation when kube-green is not running
+
+func getResourceAmountWithoutKG(resource string) string {
+	return fmt.Sprintf("(%s) * (%s)", resource, kg_not_running)
+}
+
+var cpu_co2_emission_no_kg = fmt.Sprintf("(%s) * (%f * (%s)) * %f * %f", getResourceAmountWithoutKG(cpu_usage), avg_watts, getResourceAmountWithoutKG(cpu_utilization_per_hour), pue_coeficient, emission_factors_coeficient)
+
+var memory_co2_emission_no_kg = fmt.Sprintf("(%s) * %f", getResourceAmountWithoutKG(memory_usage), memory_coefficient)
+
+var co2_emission_no_kg = fmt.Sprintf("(%s + %s) * %f * %f", cpu_co2_emission_no_kg, memory_co2_emission_no_kg, pue_coeficient, emission_factors_coeficient)
+
+///////////////////
+/*
 func decreaseAtCertainHour(startHour int, endHour int, query string, scaleCoef float32) string {
 	newQuery := fmt.Sprintf(
 		"(%s * %f and  (absent(hour() < %d) or absent(hour() > %d))) or (%s and (absent(hour() > %d) or absent(hour() < %d)))",
 		query, scaleCoef, startHour, endHour, query, startHour, endHour)
 	return newQuery
 }
-
-var co2_emission_with_kube_green = decreaseAtCertainHour(17, 6, co2_emission, 0.7)
-var saved_co2_emissions = fmt.Sprintf("(%s) - (%s)", co2_emission, co2_emission_with_kube_green)
+*/
+//var co2_emission_with_kube_green = decreaseAtCertainHour(17, 6, co2_emission, 0.7)
+//var saved_co2_emissions = fmt.Sprintf("(%s) - (%s)", co2_emission, co2_emission_with_kube_green)
 
 func sum_over_time_and_step(query string, time string, step string) string {
 	return fmt.Sprintf("sum_over_time((%s)[%s:%s])", query, time, step)
@@ -65,7 +83,7 @@ var queryDict = map[string]string{
 	"memory_usage":                 memory_usage,
 	"cpu_allocation":               cpu_allocation,
 	"co2_emission":                 co2_emission,
-	"co2_emission_with_kube_green": co2_emission_with_kube_green,
-	"saved_co2_emission":           sum_over_time_and_step(saved_co2_emissions, "1w", "1h"),
+	"co2_emission_with_kube_green": co2_emission,
+	"saved_co2_emission":           sum_over_time_and_step(co2_emission, "1w", "1h"),
 	"namespace_names":              namespace_names,
 }
