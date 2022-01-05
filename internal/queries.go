d5bd80a9f87e539226d6a39ffdc75f640cbffff7
Zoé <zoepesleuxzp@gmail.com>
2022-01-05 17:25:05 +0100

    debug

--- internal/queries.go
+++ internal/queries.go
@@ -69,7 +69,7 @@ func sum_over_time_and_step(query string, time string, step string) string {
 var number_hours_kg_not_running = sum_over_time_and_step(kg_not_running, "1w", "1h")
 var total_number_hours = fmt.Sprintf("168 - (%s)", sum_over_time_and_step(non_reliable_value, "1w", "1h"))
 
-var estimmated_co2_emission_no_kg = fmt.Sprintf("(%s) * (%s) / (%s)", sum_over_time_and_step(co2_emission_no_kg, "1w", "1h"), total_number_hours, number_hours_kg_not_running)
+var estimmated_co2_emission_no_kg = fmt.Sprintf("(%s) * 168 / (%s)", sum_over_time_and_step(co2_emission_no_kg, "1w", "1h"), number_hours_kg_not_running)
 var saved_co2_emission = fmt.Sprintf("(%s) - (%s)", estimmated_co2_emission_no_kg, sum_over_time_and_step(co2_emission, "1w", "1h"))
 
 const namespace_names = "sum(kube_namespace_labels) by (namespace)"
