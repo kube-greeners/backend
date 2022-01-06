67bcea728cb68eafb54142df8b6ca394d600c4bc
Zoé <zoepesleuxzp@gmail.com>
2022-01-06 10:21:05 +0100

    change interval from 1w to 1d

--- internal/queries.go
+++ internal/queries.go
@@ -67,11 +67,11 @@ func sum_over_time_and_step(query string, time string, step string) string {
 	return fmt.Sprintf("sum_over_time((%s)[%s:%s])", query, time, step)
 }
 
-var number_hours_kg_not_running = sum_over_time_and_step(kg_not_running, "1w", "10m")
-var total_number_hours = fmt.Sprintf("168 - (%s)", sum_over_time_and_step(non_reliable_value, "1w", "10m"))
+var number_hours_kg_not_running = sum_over_time_and_step(kg_not_running, "1d", "10m")
+var total_number_hours = fmt.Sprintf("168 - (%s)", sum_over_time_and_step(non_reliable_value, "1d", "10m"))
 
-var estimmated_co2_emission_no_kg = fmt.Sprintf("(%s) * 1008 / (%s)", sum_over_time_and_step(co2_emission_no_kg, "1w", "10m"), number_hours_kg_not_running)
-var saved_co2_emission = fmt.Sprintf("(%s) - (%s)", estimmated_co2_emission_no_kg, sum_over_time_and_step(co2_emission, "1w", "10m"))
+var estimmated_co2_emission_no_kg = fmt.Sprintf("(%s) * 144 / (%s)", sum_over_time_and_step(co2_emission_no_kg, "1d", "10m"), number_hours_kg_not_running)
+var saved_co2_emission = fmt.Sprintf("(%s) - (%s)", estimmated_co2_emission_no_kg, sum_over_time_and_step(co2_emission, "1d", "10m"))
 
 const namespace_names = "sum(kube_namespace_labels) by (namespace)"
 
